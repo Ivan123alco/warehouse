e86b25661f8c88e9deff5b37dc1c2dee235a3278
ivan.zorita.bote@gmail.com <ivan.zorita.bote@gmail.com>
2021-04-12 23:55:29 +0200

    Add endpoint to upload products and inventory via file

---
+++ warehouse-back/file-service.js
@@ -0,0 +1,45 @@
+require("dotenv").config()
+const fs = require('fs');
+
+const inventoryPath = process.env.INVENTORY_PATH;
+const productsPath = process.env.PRODUCTS_PATH;
+
+const writeInventory = items => {
+  const inventory = fileExistsSync(inventoryPath) ? JSON.parse(fs.readFileSync(inventoryPath)) : {};
+  const updatedItems = items.reduce((map, { art_id, name, stock }) => {
+    console.log(map)
+    if (!inventory[art_id]) {
+      map[art_id] = { art_id, name, stock }
+    } else {
+      map[art_id] = { art_id, name, stock: parseInt(stock) + parseInt(inventory[art_id].stock) }
+    }
+    return map
+  }, {})
+  const inventorySaved = { ...inventory, ...updatedItems }
+  fs.writeFileSync(inventoryPath, JSON.stringify(inventorySaved))
+  return inventorySaved;
+}
+
+const writeProducts = newProducts => {
+  const products = fileExistsSync(productsPath) ? JSON.parse(fs.readFileSync(productsPath)) : {};
+  const updatedProducts = newProducts.reduce((map, product) => {
+    map[product.name] = product
+    return map
+  }, {})
+  const productSaved = { ...products, ...updatedProducts }
+  fs.writeFileSync(productsPath, JSON.stringify(productSaved))
+  return productSaved;
+}
+
+
+
+const fileExistsSync = (file) => {
+  try {
+    fs.accessSync(file, fs.constants.R_OK | fs.constants.W_OK);
+    return true;
+  } catch (err) {
+    return false;
+  }
+}
+
+module.exports = { writeInventory, writeProducts }
--- warehouse-back/index.js
+++ warehouse-back/index.js
@@ -1,10 +1,68 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const _ = require('lodash');
+const { writeInventory, writeProducts } = require("./file-service")
+require('dotenv').config()
 
-app.get('/', function(req, res) {
-  res.send('Hola Mundo!');
+const app = express();
+
+// enable files upload
+app.use(fileUpload({
+  createParentPath: true
+}));
+
+//add other middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/add-inventory', async (req, res, next) => {
+  try {
+    if (!req.files && !req.files.inventory) {
+      let err = new Error('Missing required param: inventory');
+      err.statusCode = 400
+      next(err);
+    }
+    //Use the name of the input field (i.e. "avatar") to retrieve the uploaded file
+    const { inventory: items } = JSON.parse(req.files.inventory.data);
+    const inventory = writeInventory(items)
+
+    //send response
+    res.status(200).json(inventory);
+
+  } catch (err) {
+    console.error(err)
+    let error = new Error('Internal server error')
+    err.statusCode = 500
+    next(error);
+  }
 });
 
-app.listen(3000, function() {
-  console.info('Running warehous app on port 3000');
+app.post('/add-products', async (req, res, next) => {
+  try {
+    if (!req.files && !req.files.products) {
+      let err = new Error('Missing required param: products');
+      err.statusCode = 400
+      next(err);
+    }
+    //Use the name of the input field (i.e. "avatar") to retrieve the uploaded file
+    const { products: items } = JSON.parse(req.files.products.data);
+    const products = writeProducts(items)
+
+    //send response
+    res.status(200).json(products);
+
+  } catch (err) {
+    console.error(err)
+    let error = new Error('Internal server error')
+    err.statusCode = 500
+    next(error);
+  }
+});
+
+app.listen(3000, function () {
+  console.info('Running warehouse app on port 3000');
 });

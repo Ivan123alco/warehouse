ca556cbbffdbfa7589335f4f3b8dbb18305f509d
ivan.zorita.bote@gmail.com <ivan.zorita.bote@gmail.com>
2021-04-16 12:00:27 +0200

    Add instructions

--- warehouse-front/src/App.vue
+++ warehouse-front/src/App.vue
@@ -2,23 +2,11 @@
   <v-app>
     <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+        <v-icon large color="white darken-2" class="shrink ml-2 mr-2">
+          mdi-warehouse
+        </v-icon>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <h1 class="h1">Warehouse</h1>
       </div>
     </v-app-bar>
 
